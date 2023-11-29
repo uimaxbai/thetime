b948e5bf4eabf2b33a9c7735e0beef69d6d1e81f
ohiosigma1 <151540300+ohiosigma1@users.noreply.github.com>
2023-11-29 16:16:59 +0000

    d

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from 'next'
-// import { Inter } from 'next/font/google'
+import { Rubik, Inter, Outfit, JetBrains_Mono } from 'next/font/google'
 import './globals.css'
 
-// const inter = Inter({ subsets: ['latin'] })
+const rubik = Rubik({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] })
+const outfit = Outfit({ subsets: ['latin'] })
+const mono = JetBrains_Mono({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'The Time',
@@ -16,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={mono.className}>{children}</body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -23,11 +23,12 @@ export default function Home() {
   let m = ('0' + date.getMinutes()).slice(-2);
   let h = ('0' + date.getHours()).slice(-2);
   let dateStr = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + "T" + h + ":" + m + ":" + s + "." + ms
-  let dateStr1 = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + " " + h + ":" + m + ":" + s + "." + ms
+  let dateStr1 = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear()
+  let timeStr = h + ":" + m + ":" + s + "." + ms
   return (
     <main className="main flex min-h-screen flex-col items-center justify-center p-4">
       <div>
-        <h1 className="timeStr"><time dateTime={dateStr} suppressHydrationWarning={true}>{dateStr1}</time></h1>
+        <h1 className="timeStr"><time dateTime={dateStr} className="timeEl flex flex-col" suppressHydrationWarning><span className="dateString" suppressHydrationWarning>{dateStr1}</span><span suppressHydrationWarning>{timeStr}</span></time></h1>
         <div className="info-div flex gap-2 flex-row justify-items-start w-full">
           <span id="timezone" className="timezone"></span>
           <span></span>
