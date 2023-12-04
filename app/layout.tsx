16fdb18cb0dba6e208269d12a3ae7debe3fefbab
DownloadFirefox <61615730+downloadfirefox@users.noreply.github.com>
2023-12-04 18:22:21 +0000

    hi

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,14 @@ const mono = JetBrains_Mono({ subsets: ['latin'] })
 <link rel="manifest" href="/manifest.json" />
 <link rel="apple-touch-icon" href="/icon.png"></link>
 <meta name="theme-color" content="#fff" />
+
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#2b5797">
+<meta name="theme-color" content="#ffffff">
 */
 
 export const viewport: Viewport = {
@@ -23,7 +31,9 @@ export const metadata: Metadata = {
   description: 'Literally just the time.',
   keywords: 'time, thetime, current time',
   manifest: '/manifest.webmanifest',
-  apple: "/icon.png"
+  mask: "/safari-pinned-tab.svg",
+  themeColor: "#000000",
+  TileColor: "#2b5797"
 }
 
 export default function RootLayout({
